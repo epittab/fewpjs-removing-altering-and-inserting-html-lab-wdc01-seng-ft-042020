ee00cb0def4dc09783ec49fd4f7cc5c602914e14
epittab <ep7m@me.com>
2020-05-31 14:17:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const main = document.querySelector('main');
+main.remove();
+
+
+let header = document.createElement('h1');
+document.body.appendChild(header);
+
+
+let newHeader = document.body.querySelector('h1');
+newHeader.id = "victory";
+newHeader = document.body.querySelector('h1#victory');
+
+
+const myName = 'Enrique Pittaluga';
+newHeader.innerHTML = `${myName} is the champion`;
